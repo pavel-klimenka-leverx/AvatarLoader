df9353186c67fea0cca1c6c88bd430bd224709cb
Pavel Klimenka <pavel.klimenka@gmail.com>
2023-07-28 13:24:19 +0300

    add additional info to user profile model

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -1,6 +1,14 @@
 ﻿
 namespace AvatarTemp.Models
 {
+    public enum TechnicalLevel
+    {
+        D1, //Junior, Junior+
+        D2, //Middle, Middle+
+        D3, //Senior, Senior+
+        D4  //Team Lead, Tech Lead
+    }
+
     public class UserProfile
     {
         public string? Id { get; set; }
@@ -9,11 +17,15 @@ namespace AvatarTemp.Models
         public DateOnly Birthday { get; set; }
         public DateOnly EmploymentDate { get; set; }
         public int EnglishLevel { get; set; }
-        public Guid OfficeLocationId { get; set; }
         public string? RoomNumber { get; set; }
         public string? Phone { get; set; }
         public string? Email { get; set; }
         public string? Position { get; set; }
         public string? ImageUrl { get; set; }
+        public Guid? LesUserProfileId { get; set; }
+        public Guid DepartmentId { get; set; }
+        public Guid OfficeLocationId { get; set; }
+        public Guid? UnitId { get; set; }
+        public TechnicalLevel TechnicalLevel { get; set; }
     }
 }
